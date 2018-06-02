ff1834b06d0537457269b2708445c44e5d2f5252
Pamela Frieze <pfrieze@gmail.com>
2018-06-02 02:12:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,7 @@ class Dog
   end
 
   def self.find_or_create_by(name, breed)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, album)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
   end
 
   def self.find_by_name(name)
