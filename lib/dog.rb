650c8b10ec5e1d1fbd77027384bed729d57af395
Pamela Frieze <pfrieze@gmail.com>
2018-06-02 02:13:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,7 @@ class Dog
 
     if !dog.empty?
       dog_data = dog[0]
-      dog = Song.new(song_data[0], song_data[1], song_data[2])
+      dog = Dog.new(song_data[0], song_data[1], song_data[2])
     else
       song = self.create(name: name, album: album)
     end
